c74f3626808b53f40783090d223b640da42a5aa5
Tung Nguyen <ngtientung98@gmail.com>
2022-04-13 20:52:39 +0700

    f

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,5 +1,19 @@
 import { Transform } from 'class-transformer';
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsEnum, IsNotEmpty } from 'class-validator';
+
+enum Category {
+  DONG_TRUNG_HA_THAO = 'DONG_TRUNG_HA_THAO',
+  YEN_SAO_THUONG_HANG = 'YEN_SAO_THUONG_HANG',
+  SAFFARON = 'SAFFARON',
+  NHAN_SAM = 'NHAN_SAM',
+  KHAC = 'KHAC',
+}
+
+enum ProductStates {
+  AVAILABLE = 'AVAILABLE',
+  SOLD_OUT = 'SOLD_OUT',
+  COMING_SOON = 'COMING_SOON',
+}
 
 export class CreateProductDto {
   @IsNotEmpty()
@@ -17,4 +31,10 @@ export class CreateProductDto {
 
   @IsNotEmpty()
   images: string[];
+
+  @IsEnum(Category)
+  category: Category;
+
+  @IsEnum(ProductStates)
+  state: ProductStates;
 }
