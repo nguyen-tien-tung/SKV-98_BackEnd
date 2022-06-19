4bdf51c1c95861b55b4f2c9d30cf2896e2f587c2
Tung Nguyen <ngtientung98@gmail.com>
2022-06-19 15:18:59 +0700

    hot fix create

--- src/user/users.controller.ts
+++ src/user/users.controller.ts
@@ -22,28 +22,24 @@ import { ReturnUserDto } from './dto/return-user.dto';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('user')
-@UseGuards(JwtAuthGuard)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Post()
-  async create(
-    @Body() userData: CreateUserDto,
-    @Res() res: Response,
-  ): Promise<ReturnUserDto> {
+  async create(@Body() userData: CreateUserDto, @Res() res: Response) {
     try {
       const salt = await bcrypt.genSalt();
       userData.password = await bcrypt.hash(userData.password, salt);
       const userCreated = await this.usersService.createUser(userData);
       // if (userCreated) {
       const { password, ...rest } = userCreated;
-      return rest;
+      res.send(rest);
       // }
     } catch (e) {
       if (e instanceof Prisma.PrismaClientKnownRequestError) {
         if (e.code === 'P2002') {
           console.log(
-            'There is a unique constraint violation, a new user cannot be created with this email',
+            'There is a unique constraint violation, a new user cannot be created ',
           );
         }
       }
@@ -55,17 +51,17 @@ export class UsersController {
       // throw e;
     }
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return this.usersService.user({ id });
   }
-
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(): Promise<ReturnUserDto[]> {
     return this.usersService.users({});
   }
-
+  @UseGuards(JwtAuthGuard)
   @Patch()
   async updateUser(@Request() req: any) {
     try {
@@ -81,7 +77,7 @@ export class UsersController {
       });
     } catch (error) {}
   }
-
+  @UseGuards(JwtAuthGuard)
   @Delete()
   deleteUser(@Body() data) {
     return this.usersService.deleteUser(data);
